e95334afe3267e502213b012f8d819b1948e083a
CheshirLvova <ksoovnaila@gmail.com>
2021-10-03 23:42:01 +0300

    Rename

--- server.py
+++ server.py
@@ -7,13 +7,24 @@ gc = geonamescache.GeonamesCache()
 
 @app.route('/')
 def index():
-    return render_template('template.html',
-        data=[value['name'] for key, value in gc.get_cities().items()])
+    return render_template('index.html',
+        data={'cities': [value['name'] for key, value in gc.get_cities().items()],
+              'city': None,
+              'total_cases': 360.524,
+              'active_cases': 244.375,
+              'recovered': 100.658,
+              'total_death': 15.491})
 
-@app.route('/test' , methods=['GET', 'POST'])
-def test():
-    select = request.form.get('comp_select')
-    return(str(select)) # just to see what select is
+@app.route('/calculate' , methods=['GET', 'POST'])
+def calculate():
+    selected_city = str(request.form.get('comp_select'))
+    return render_template('index.html',
+        data={'cities': [value['name'] for key, value in gc.get_cities().items()],
+              'city': selected_city,
+              'total_cases': 360.524,
+              'active_cases': 244.375,
+              'recovered': 100.658,
+              'total_death': 15.491})
 
 if __name__=='__main__':
     app.run(debug=True)
